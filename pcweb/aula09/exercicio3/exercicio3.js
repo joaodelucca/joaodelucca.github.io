5c6cfec9bd2e520f6a347fb76f8b1c3457ff7e98
joao <joaodeluccajp@gmail.com>
2022-07-22 23:02:53 -0300

    arrays

--- pcweb/aula09/exercicio3/exercicio3.js
+++ pcweb/aula09/exercicio3/exercicio3.js
@@ -1,6 +1,31 @@
-let num = 575;
-
-function troca(num) {
-    let romano = [I, V, X, L, C, D, M];
-
+function roma(numero2) {
+    let vetor = [
+        [1000, "M"],
+        [900, "CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"]
+    ];
+    let resultado = []
+    for (let value of vetor) {
+        let i = parseInt(numero2 / value[0]);
+        numero2 = numero2 % value[0];
+        if (i) {
+            resultado.push(value[1].repeat(i));
+        }
+    }
+    return resultado.join("");
 }
+document.getElementById("b1").addEventListener("click", () => {
+    const numero1 = document.getElementById("numero");
+    const imprim = document.getElementById("imprimir");
+    imprim.innerHTML = roma(parseInt(numero1.value));
+})
---
+++ pcweb/aula09/exercicio4/exercicio4.js
@@ -0,0 +1,29 @@
+function notas(dinheiro) {
+    let total = [];
+    if (dinheiro % 2 == 0) {
+        let vetor = [100, 50, 20, 10, 2, 5];
+        for (let nota of vetor) {
+            let i = parseInt(dinheiro / nota);
+            if (i) {
+                total.push([nota, i]);
+            }
+            dinheiro -= i * nota;
+        }
+        document.write(total);
+    } else {
+        let vetor = [100, 50, 20, 10, 5, 2];
+        for (let nota of vetor) {
+            let i = parseInt(dinheiro / nota);
+            if (i) {
+                total.push([nota, i]);
+            }
+            dinheiro -= i * nota;
+        }
+    }
+    return total;
+}
+document.getElementById("b1").addEventListener("click", () => {
+    const numero1 = document.getElementById("numero");
+    const imprim = document.getElementById("imprimir");
+    imprim.innerText = notas(parseInt(numero1.value));
+})
